0a734e94149172bfd3019cf03ac3a3e74f395ac1
Wesley Luyten <me@wesleyluyten.com>
2024-12-18 17:47:11 -0600

    feat: use ce-la-react, fix React 19 issues (#61)

--- examples/nextjs/app/dash-video/page.tsx
+++ examples/nextjs/app/dash-video/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import DashVideo from 'dash-video-element/react';
+import Player from '../player';
 
 export const metadata: Metadata = {
   title: 'Dash Video - Media Elements',
@@ -9,13 +10,14 @@ export default function Page() {
   return (
     <>
       <section>
-        <DashVideo
+        <Player
+          as={DashVideo}
           className="video"
           src="https://player.vimeo.com/external/648359100.mpd?s=a4419a2e2113cc24a87aef2f93ef69a8e4c8fb0c"
           poster="https://image.mux.com/jtWZbHQ013SLyISc9LbIGn8f4c3lWan00qOkoPMZEXmcU/thumbnail.webp?time=0"
           controls
           playsInline
-        ></DashVideo>
+        ></Player>
       </section>
     </>
   );
