089434504e72c8b2ffcc7e63dae0e85888b7ed52
Wesley Luyten <me@wesleyluyten.com>
2025-03-13 15:19:51 -0500

    fix: YT config on React (SSR) + config types (#92)

--- packages/youtube-video-element/youtube-video-element.d.ts
+++ packages/youtube-video-element/youtube-video-element.d.ts
@@ -7,4 +7,20 @@ export default class CustomVideoElement extends HTMLVideoElement {
   ): void;
   connectedCallback(): void;
   disconnectedCallback(): void;
+  config: {
+    cc_lang_pref?: string;
+    cc_load_policy?: 1;
+    color?: 'red' | 'white';
+    disablekb?: 0 | 1;
+    enablejsapi?: 0 | 1;
+    end?: number;
+    fs?: 0 | 1;
+    hl?: string;
+    iv_load_policy?: 1 | 3;
+    origin?: string;
+    playlist?: string;
+    rel?: 0 | 1;
+    start?: number;
+    widget_referrer?: string;
+  }
 }
