6a0c55f8fe39aa8859cfb83c35fae8e3e3e1fe9d
ronalduQualabs <ronaldu@qualabs.com>
2025-07-22 15:56:13 -0300

    feat:: Enable multiple renditions in dash video element

--- packages/dash-video-element/dash-video-element.js
+++ packages/dash-video-element/dash-video-element.js
@@ -1,6 +1,7 @@
 import { CustomVideoElement } from 'custom-media-element';
+import { MediaTracksMixin } from 'media-tracks';
 
-class DashVideoElement extends CustomVideoElement {
+class DashVideoElement extends MediaTracksMixin(CustomVideoElement) {
   static shadowRootOptions = { ...CustomVideoElement.shadowRootOptions };
 
   static getTemplateHTML = (attrs) => {
@@ -21,19 +22,46 @@ class DashVideoElement extends CustomVideoElement {
   }
 
   async load() {
-
-    if (!this.#apiInit) {
-      this.#apiInit = true;
-
-      const Dash = await import('dashjs');
-
-      this.api = Dash.MediaPlayer().create();
-      this.api.initialize(this.nativeEl, this.src, this.autoplay);
-
-    } else {
-
+    if (this.#apiInit) {
       this.api.attachSource(this.src);
+      return;
     }
+
+    this.#apiInit = true;
+
+    const Dash = await import('dashjs');
+    this.api = Dash.MediaPlayer().create();
+    this.api.initialize(this.nativeEl, this.src, this.autoplay);
+
+    this.api.on(Dash.MediaPlayer.events.STREAM_INITIALIZED, () => {
+      const bitrateList = this.api.getRepresentationsByType('video');
+
+      let videoTrack = this.videoTracks.getTrackById('main');
+      if (!videoTrack) {
+        videoTrack = this.addVideoTrack('main');
+        videoTrack.id = 'main';
+        videoTrack.selected = true;
+      }
+
+      bitrateList.forEach((rep) => {
+        const bitrate =
+          rep.bandwidth ?? rep.bitrate ?? (Number.isFinite(rep.bitrateInKbit) ? rep.bitrateInKbit * 1000 : undefined);
+
+        const rendition = videoTrack.addRendition(rep.id, rep.width, rep.height, rep.mimeType ?? rep.codec, bitrate);
+        rendition.id = rep.id;
+      });
+
+      this.videoRenditions.addEventListener('change', () => {
+        const selected = this.videoRenditions[this.videoRenditions.selectedIndex];
+
+        if (selected?.id) {
+          this.api.updateSettings({ streaming: { abr: { autoSwitchBitrate: { video: false } } } });
+          this.api.setRepresentationForTypeById('video', selected.id, true);
+        } else {
+          this.api.updateSettings({ streaming: { abr: { autoSwitchBitrate: { video: true } } } });
+        }
+      });
+    });
   }
 }
 
