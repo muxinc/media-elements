48b84cad1852516f82ed6d3e2716b4948bbb32a1
ronalduQualabs <ronaldu@qualabs.com>
2026-01-13 12:02:36 -0300

    types update

--- packages/media-played-ranges-mixin/index.d.ts
+++ packages/media-played-ranges-mixin/index.d.ts
@@ -1,10 +1,45 @@
-type Constructor<T = {}> = new (...args: any[]) => T;
+export interface PlayedRange {
+  start: number;
+  end: number;
+}
 
-type TimeRange = { start: number; end: number };
+export interface MediaPlayedRangesMixinHost {
+  currentTime: number;
+  paused: boolean;
+}
 
-export function MediaPlayedRangesMixin<T extends Constructor>(Base: T): T & {
-  new (...args: any[]): {
+export type Constructor<T = {}> = new (...args: any[]) => T;
+
+export interface PlaybackEventParam {
+  time?: number;
+}
+
+export function MediaPlayedRangesMixin<
+  TBase extends Constructor<MediaPlayedRangesMixinHost>
+>(
+  Base: TBase
+): Constructor<
+  InstanceType<TBase> & {
+    /** Internal merged played ranges */
+    _playedRanges: PlayedRange[];
+
+    /** Currently accumulating range */
+    _currentPlayedRange: PlayedRange | null;
+
+    /** Merge tolerance (seconds) */
+    _RANGE_EPSILON: number;
+
+    /** Whether the media is currently seeking */
+    _seeking: boolean;
+
+    onPlaybackStart(param?: PlaybackEventParam): void;
+    onSeeking(): void;
+    onSeeked(param?: PlaybackEventParam): void;
+    onPlaybackStop(param?: PlaybackEventParam): void;
+
     addPlayedRange(start: number, end: number): void;
+
+    /** TimeRanges-like representation of played media */
     readonly played: TimeRanges;
-  };
-};
+  }
+>;
